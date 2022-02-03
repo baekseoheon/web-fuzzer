3302effdc572ab31d0295726959091317dbfa5f1
sean-baek <qtt1536@gmail.com>
2022-02-03 01:56:31 -0500

    update func

--- src/func.py
+++ src/func.py
@@ -11,6 +11,9 @@ import concurrent.futures
 import urllib.parse as p 
 import urllib.error as e
 import requests.exceptions as reqe
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from seleniumrequests import Chrome
 
 wl_file = 'wordlist.txt'
 ext = [".php", ".txt"]
@@ -123,6 +126,80 @@ def dir_scan(target, wordlist, extensions=None):
                     print("!!! [{}] ==> {}".format(er.HTTPError.code, url))
         word = f.readline()
     f.close()
+def xss_scan(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver.implicitly_wait(time_to_wait=10)
+    driver.get(url=url)
+    
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    for formtag in soup.find_all('form'):
+        print(formtag)
+        if (formtag.get('method').upper() == 'GET'):
+            print("Using GET method")
+            with open('xss_payload.txt', 'r', errors="error") as code:
+                for i in code:
+                    for inputtag in soup.find_all('input'):
+                        try:
+                            header = {"User-Agent": user_agent}
+                            inputtagname = inputtag.get('name')
+                            if formtag.get('action'):
+                                action = formtag.get('action')
+                                payload = delencode(url + '/' + action + '?' + inputtagname + '=' + i)
+                                #res = requests.get(payload)
+                                driver.get(url=payload)
+                                current_url = driver.current_url
+                                print(current_url)
+
+                                try:
+                                    alert = driver.switch_to_alert
+                                    alert.accept()
+
+                                    print("Vulnerable payload find\t: " + current_url)
+                                    with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                        rf.write(payload+"\n")
+                                except: pass
+
+                                '''
+                                if i in res.text:
+                                    #print("parameter vulnerable")
+                                    print("Vulnerable payload find\t: " + res.url)
+                                    with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                        rf.write(payload+"\n")
+                                else: print("Trying\t => [" + res.url + "]")
+                                '''
+                        except: pass
+        elif (formtag.get('method').upper() == 'POST'):
+            print("Using POST method")
+            with open('xss_payload.txt', 'r', errors='error') as code:
+                i = code.readline()
+                print(i)
+                #for i in code:
+                for inputtag in soup.findAll('input'):
+                    inputtagname = inputtag.get('name')
+                    if inputtagname is None:
+                        continue
+                    data = {}
+                    data[inputtagname] = i
+                    #print("input tag name : {}, payload : {}".format(inputtagname, data[inputtagname]))
+                    try:
+                        header = {"User-Agent" : user_agent}
+                        payload = delencode(url + '/' + formtag.get('action'))
+                        print("payload ", payload)
+                        rep = requests.post(payload, headers=header, data=data)
+                        
+                        if rep.status_code == 200:
+                            print("Vulneranle Payload Find\t: " + rep.url)
+                            with open("result_xss_scan/" + delschema(url) + '_xss_post.txt', "a+") as rf:
+                                rf.write(payload + "\n" + inputtagname + ":" + i + '\n')
+                        else:
+                            print("Trying\t:", rep.url)
+                    except: pass
+    driver.close()
 
 def web_scan(url):
     res = requests.get(url)
