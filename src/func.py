fc5eb014e83b0953f19166c9354bdba8a4ff209e
sean-baek <qtt1536@gmail.com>
2022-02-09 05:55:02 -0500

    update

--- src/func.py
+++ src/func.py
@@ -129,30 +129,101 @@ def dir_scan(target, wordlist, extensions=None):
 
 def xss_scan_v2(url):
     options = webdriver.ChromeOptions()
-    #options.add_argument('headless')
-    #options.add_argument('window-size=1920x1080')
-    #options.add_argument('--disable-gpu')
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument('--disable-gpu')
     options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
 
-    print("1")
     driver = webdriver.Chrome('./chromedriver', options=options)
-    print("2")
     driver.implicitly_wait(time_to_wait=10)
-    print("1")
-    driver.get(url=url)
-    print("1")
+    driver.get(url)
 
     src = driver.page_source
     soup = BeautifulSoup(src, "html.parser")
 
     for formtag in soup.find_all('form'):
-        if formtag.get('type'):
-            formtagtype = formtag.get('type')
-            if formtagtype != None:
-                if formtagtype.upper() != 'text': continue 
-                else: pass
-            else: pass
         print(formtag)
+        if(formtag.get('method').upper() == "GET"):
+            with open('xss_payload.txt', 'r', errors="error") as code:
+                for i in code:
+                    print("2")
+                    for inputtag in soup.find_all('input'):
+                        print("3")
+                        try:
+                            print("4")
+                            header = {"User-Agent": user_agent}
+                            inputtagname = inputtag.get('name')
+                            print(inputtagname)
+                            if formtag.get('action'):
+                                action = formtag.get('action')
+                                payload = delencode(url + '/' + action + '?' + inputtagname + '=' + i)
+                            else: payload = delencode(url + '?' + inputtagname + '=' + i)
+
+                            #res = requests.get(payload)
+                            driver.get(payload)
+                            current_url = driver.current_url
+                            #print(current_url)
+
+                            try:
+                                print("5")
+                                alert = driver.switch_to.alert
+                                alert.accept()
+                                print("6")
+
+                                print("Vulnerable payload find\t: " + current_url)
+
+                                if not os.path.isdir("result_xss_scan"):
+                                    os.mkdir("result_xss_scan")
+
+                                with open("result_xss_scan/" + delschema(url) + "_xss_get.txt", "a+") as rf:
+                                    rf.write(payload+'\n')
+                            except Exception as e1:
+                                print("inside excepted ", e1)
+
+                            '''
+                            if i in res.text:
+                                #print("parameter vulnerable")
+                                print("Vulnerable payload find\t: " + res.url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            else: print("Trying\t => [" + res.url + "]")
+                            '''
+                        except Exception as e:
+                            print("excepted ", e)
+
+        elif (formtag.get('method').upper() == 'POST'): # form 태그의 method가 POST 방식일 때
+            print("Using POST method")
+            with open('xss_payload.txt', 'r', errors='error') as code:
+                #i = code.readline()
+                #print(i)
+                for i in code:
+                    for inputtag in soup.find_all('input'):
+                        inputtagname = inputtag.get('name')
+                        if inputtagname is None:
+                            continue
+                        data = {}
+                        data[inputtagname] = i
+                        #print("input tag name : {}, payload : {}".format(inputtagname, data[inputtagname]))
+                        try:
+                            header = {"User-Agent" : user_agent}
+                            payload = delencode(url + '/' + formtag.get('action'))
+                            print("payload ", payload)
+                            rep = requests.post(payload, headers=header, data=data)
+                        
+                            if rep.status_code == 200:
+                                print("Vulneranle Payload Find\t: " + rep.url)
+                                if not os.path.isdir("result_xss_scan"):
+                                    os.mkdir("result_xss_scan")
+
+                                with open("result_xss_scan/" + delschema(url) + "_xss_post.txt", "a+") as rf:
+                                    rf.write(payload + '\n' + "input tag name : " + inputtagname + "\n" + "payload : " + i + '\n')
+                            else:
+                                print("Trying\t:", rep.url)
+                        except Exception as e:
+                            print("POST Exception : ", e)
+
+
+        #elif (formtag.get('method').upper() == "POST"):
     driver.close
 
 
@@ -233,7 +304,8 @@ def xss_scan(url):
                                 rf.write(payload + "\n" + inputtagname + ":" + i + '\n')
                         else:
                             print("Trying\t:", rep.url)
-                    except: pass
+                    except Exception as e:
+                        print("POST Exception : ", e)
     driver.close()
 
 def web_scan(url):
