457e908f8bb12efb70e5fe6203d2935f1e8bd888
sean-baek <qtt1536@gmail.com>
2022-02-09 15:36:14 +0900

    update

--- src/func.py
+++ src/func.py
@@ -126,6 +126,40 @@ def dir_scan(target, wordlist, extensions=None):
                     print("!!! [{}] ==> {}".format(er.HTTPError.code, url))
         word = f.readline()
     f.close()
+
+def xss_scan_v2(url):
+    options = webdriver.ChromeOptions()
+    #options.add_argument('headless')
+    #options.add_argument('window-size=1920x1080')
+    #options.add_argument('--disable-gpu')
+    options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
+
+    print("1")
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    print("2")
+    driver.implicitly_wait(time_to_wait=10)
+    print("1")
+    driver.get(url=url)
+    print("1")
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    for formtag in soup.find_all('form'):
+        if formtag.get('type'):
+            formtagtype = formtag.get('type')
+            if formtagtype != None:
+                if formtagtype.upper() != 'text': continue 
+                else: pass
+            else: pass
+        print(formtag)
+    driver.close
+
+
+    
+
+
+    
 def xss_scan(url):
     options = webdriver.ChromeOptions()
     options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
@@ -137,9 +171,9 @@ def xss_scan(url):
     src = driver.page_source
     soup = BeautifulSoup(src, "html.parser")
 
-    for formtag in soup.find_all('form'):
+    for formtag in soup.find_all('form'): # form 태그 가져오기
         print(formtag)
-        if (formtag.get('method').upper() == 'GET'):
+        if (formtag.get('method').upper() == 'GET'): # form 태그의 method가 GET 방식일 때
             print("Using GET method")
             with open('xss_payload.txt', 'r', errors="error") as code:
                 for i in code:
@@ -150,30 +184,31 @@ def xss_scan(url):
                             if formtag.get('action'):
                                 action = formtag.get('action')
                                 payload = delencode(url + '/' + action + '?' + inputtagname + '=' + i)
-                                #res = requests.get(payload)
-                                driver.get(url=payload)
-                                current_url = driver.current_url
-                                print(current_url)
+                            else: payload = delencode(url + '?' + inputtagname + '=' + i)
+                            #res = requests.get(payload)
+                            driver.get(url=payload)
+                            current_url = driver.current_url
+                            print(current_url)
 
-                                try:
-                                    alert = driver.switch_to_alert
-                                    alert.accept()
+                            try:
+                                alert = driver.switch_to_alert
+                                alert.accept()
 
-                                    print("Vulnerable payload find\t: " + current_url)
-                                    with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
-                                        rf.write(payload+"\n")
-                                except: pass
+                                print("Vulnerable payload find\t: " + current_url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            except: pass
 
-                                '''
-                                if i in res.text:
-                                    #print("parameter vulnerable")
-                                    print("Vulnerable payload find\t: " + res.url)
-                                    with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
-                                        rf.write(payload+"\n")
-                                else: print("Trying\t => [" + res.url + "]")
-                                '''
+                            '''
+                            if i in res.text:
+                                #print("parameter vulnerable")
+                                print("Vulnerable payload find\t: " + res.url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            else: print("Trying\t => [" + res.url + "]")
+                            '''
                         except: pass
-        elif (formtag.get('method').upper() == 'POST'):
+        elif (formtag.get('method').upper() == 'POST'): # form 태그의 method가 POST 방식일 때
             print("Using POST method")
             with open('xss_payload.txt', 'r', errors='error') as code:
                 i = code.readline()
