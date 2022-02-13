9c56c22cf2b489168f503015ba6e06f743f6f793
sean-baek <root@localhost>
2022-02-14 01:23:38 +0900

    final 1

---
+++ src/old_file_backup/func_backup.py
@@ -0,0 +1,326 @@
+#!/bin/python3
+
+import os
+import re
+import sys
+import queue
+import time
+import urllib3
+import requests
+import concurrent.futures
+import urllib.parse as p 
+import urllib.error as e
+import requests.exceptions as reqe
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from seleniumrequests import Chrome
+
+wl_file = 'wordlist.txt'
+ext = [".php", ".txt"]
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36'
+
+def delencode(url):
+    url = url.replace("#", "")
+    url = url.replace("%0A", "")
+    url = url.replace("\n", "")
+    url = url.replace("%40", "@")
+    return url
+
+def delschema(url):
+    filename = url.replace("https://", "")
+    filename = url.replace("http://", "")
+    filename = re.sub("\/.*", "", filename)
+    return filename
+
+def create_wordlist(wl_file):
+    is_resume = False
+    words = queue.Queue()
+    print("------------------------------")
+    print("created wordlist queue")
+    print("------------------------------\n")
+
+    try:
+        with open(wl_file) as fp:
+            print("------------------------------")
+            print("file open succeed")
+            print("------------------------------\n")
+        
+            dict_names = fp.readline()
+            print("------------------------------")
+            print("a line was read from the file ")
+            print("------------------------------\n")
+            while dict_names:
+                dict_name_word = dict_names.strip()
+                words.put(dict_name_word)
+                dict_names = fp.readline()
+            print("------------------------------")
+            print("created wordlist queue")
+            print("------------------------------\n")
+
+        fp.close()
+        return words
+    except:
+        print("there is no file : %s" % wl_file)
+        sys.exit(0)
+
+def dir_scan(target, wordlist, extensions=None):
+    f = open(wordlist, "r")
+    
+    word = f.readline()
+    while word != '':
+        if word == '\n':
+            word = f.readline()
+            continue
+
+        if word == '\0':
+            word = f.readline()
+            continue
+
+        if '\n' in word:
+           word = word.replace("\n", "")
+
+        try_this = word
+        try_list = []
+        if extensions:
+            if '.' not in try_this:
+                if try_this == '/':
+                    try_list.append("{}".format(try_this))
+                else:
+                    try_list.append("/{}/".format(try_this))
+                    for extension in extensions:
+                        try_list.append("/{}{}".format(try_this, extension))
+            else:
+                try_list.append("/{}".format(try_this))
+        else:
+            if '.' not in try_this:
+                try_list.append("/{}/".format(try_this))
+            else:
+                try_list.append("/{}".format(try_this))
+
+        for list in try_list:
+            url = "{}{}".format(target, p.quote(list))
+            url = delencode(url)
+            
+            try:
+                headers = {
+                    'User_Agent' : user_agent
+                }
+                res = requests.get(url, headers=headers)
+                if len(res.text):
+                    if res.status_code != 404:
+                        if res.status_code == 200:
+                            print("==================================================")
+                            print("found : [{}] ==> {}".format(res.status_code, url))
+                            print("==================================================")
+                            if not os.path.isdir("result"):
+                                os.mkdir("result")
+
+                            with open("result/" + delschema(target) + "_dir_scan.txt", "a+") as ff:
+                                ff.write(url+'\n')
+                    #else: print("can not find : ", url)
+                else: print(f'there is no data : {url}\n')
+
+            except(reqe.HTTPError) as er:
+                print("except", er)
+                if hasattr(er.HTTPError, 'code') and er.HTTPError.code != 404:
+                    print("!!! [{}] ==> {}".format(er.HTTPError.code, url))
+        word = f.readline()
+    f.close()
+
+def xss_scan_v2(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument('--disable-gpu')
+    options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver.implicitly_wait(time_to_wait=10)
+    driver.get(url)
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    for formtag in soup.find_all('form'):
+       # print(formtag)
+        if(formtag.get('method').upper() == "GET"):
+            with open('xss_payload.txt', 'r', errors="error") as code:
+                for i in code:
+                    print("2")
+                    for inputtag in soup.find_all('input'):
+                        print("3")
+                        try:
+                            print("4")
+                            header = {"User-Agent": user_agent}
+                            inputtagname = inputtag.get('name')
+                            print(inputtagname)
+                            if formtag.get('action'):
+                                action = formtag.get('action')
+                                payload = delencode(url + '/' + action + '?' + inputtagname + '=' + i)
+                            else: payload = delencode(url + '?' + inputtagname + '=' + i)
+
+                            #res = requests.get(payload)
+                            driver.get(payload)
+                            current_url = driver.current_url
+                            #print(current_url)
+
+                            try:
+                                print("5")
+                                alert = driver.switch_to.alert
+                                alert.accept()
+                                print("6")
+
+                                print("Vulnerable payload find\t: " + current_url)
+
+                                if not os.path.isdir("result_xss_scan"):
+                                    os.mkdir("result_xss_scan")
+
+                                with open("result_xss_scan/" + delschema(url) + "_xss_get.txt", "a+") as rf:
+                                    rf.write(payload+'\n')
+                            except Exception as e1:
+                                print("inside excepted ", e1)
+
+                            '''
+                            if i in res.text:
+                                #print("parameter vulnerable")
+                                print("Vulnerable payload find\t: " + res.url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            else: print("Trying\t => [" + res.url + "]")
+                            '''
+                        except Exception as e:
+                            print("excepted ", e)
+
+        elif (formtag.get('method').upper() == 'POST'): # form 태그의 method가 POST 방식일 때
+            print("Using POST method")
+            with open('xss_payload.txt', 'r', errors='error') as code:
+                #i = code.readline()
+                #print(i)
+                for i in code:
+                    for inputtag in soup.find_all('input'):
+                        inputtagname = inputtag.get('name')
+                        if inputtagname is None:
+                            continue
+                        data = {}
+                        data[inputtagname] = i
+                        #print("input tag name : {}, payload : {}".format(inputtagname, data[inputtagname]))
+                        try:
+                            header = {"User-Agent" : user_agent}
+                            payload = delencode(url + '/' + formtag.get('action'))
+                            #print("payload ", payload)
+                            rep = requests.post(payload, headers=header, data=data)
+                        
+                            if payload in rep.text:
+                                print("Vulneranle Payload Find\t: " + rep.url)
+                                if not os.path.isdir("result_xss_scan"):
+                                    os.mkdir("result_xss_scan")
+
+                                with open("result_xss_scan/" + delschema(url) + "_xss_post.txt", "a+") as rf:
+                                    rf.write(payload + '\n' + "input tag name : " + inputtagname + "\n" + "payload : " + i + '\n')
+                            else:
+                                print("Trying\t:", rep.url)
+                        except Exception as e:
+                            print("POST Exception : ", e)
+
+
+        #elif (formtag.get('method').upper() == "POST"):
+    driver.close
+
+
+    
+
+
+    
+def xss_scan(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36')
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver.implicitly_wait(time_to_wait=10)
+    driver.get(url=url)
+    
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    for formtag in soup.find_all('form'): # form 태그 가져오기
+        print(formtag)
+        if (formtag.get('method').upper() == 'GET'): # form 태그의 method가 GET 방식일 때
+            print("Using GET method")
+            with open('xss_payload.txt', 'r', errors="error") as code:
+                for i in code:
+                    for inputtag in soup.find_all('input'):
+                        try:
+                            header = {"User-Agent": user_agent}
+                            inputtagname = inputtag.get('name')
+                            if formtag.get('action'):
+                                action = formtag.get('action')
+                                payload = delencode(url + '/' + action + '?' + inputtagname + '=' + i)
+                            else: payload = delencode(url + '?' + inputtagname + '=' + i)
+                            #res = requests.get(payload)
+                            driver.get(url=payload)
+                            current_url = driver.current_url
+                            print(current_url)
+
+                            try:
+                                alert = driver.switch_to_alert
+                                alert.accept()
+
+                                print("Vulnerable payload find\t: " + current_url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            except: pass
+
+                            '''
+                            if i in res.text:
+                                #print("parameter vulnerable")
+                                print("Vulnerable payload find\t: " + res.url)
+                                with open("result_xss_scan" + delschema(url) + '_xss_get.txt', "a+") as rf:
+                                    rf.write(payload+"\n")
+                            else: print("Trying\t => [" + res.url + "]")
+                            '''
+                        except: pass
+        elif (formtag.get('method').upper() == 'POST'): # form 태그의 method가 POST 방식일 때
+            print("Using POST method")
+            with open('xss_payload.txt', 'r', errors='error') as code:
+                i = code.readline()
+                print(i)
+                #for i in code:
+                for inputtag in soup.findAll('input'):
+                    inputtagname = inputtag.get('name')
+                    if inputtagname is None:
+                        continue
+                    data = {}
+                    data[inputtagname] = i
+                    #print("input tag name : {}, payload : {}".format(inputtagname, data[inputtagname]))
+                    try:
+                        header = {"User-Agent" : user_agent}
+                        payload = delencode(url + '/' + formtag.get('action'))
+                        print("payload ", payload)
+                        rep = requests.post(payload, headers=header, data=data)
+                        
+                        if rep.status_code == 200:
+                            print("Vulneranle Payload Find\t: " + rep.url)
+                            with open("result_xss_scan/" + delschema(url) + '_xss_post.txt', "a+") as rf:
+                                rf.write(payload + "\n" + inputtagname + ":" + i + '\n')
+                        else:
+                            print("Trying\t:", rep.url)
+                    except Exception as e:
+                        print("POST Exception : ", e)
+    driver.close()
+
+def web_scan(url):
+    res = requests.get(url)
+    print("==================================================")
+    print("{} URL : {}".format(res.status_code, res.url))
+    print("Headers : ", res.headers)
+    print("cookies : ", res.cookies)
+    print("==================================================")
+    if not os.path.isdir("result"):
+        os.mkdir("result")
+
+    with open("result/"+delschema(url)+"_web_scan.txt", "a+") as ff:
+        ff.write('[' + str(res.status_code) + '] ' + str(res.url) + '\n' + str(res.headers) +'\n' + str(res.cookies) + '\n\n')
+        ff.close()
+
+
+    
--- src/old_file_backup/main_backup.py
+++ src/old_file_backup/main_backup.py
@@ -1,14 +1,16 @@
-import sys, os, time, requests, logging
 from __future__ import print_function
 from ast import arg
 from re import X
+import sys
+import requests
+import time
 from pprint import pprint
 from text_color import *
 from banner import *
 from arg_parse import *
 from usage import *
-from func import *
-from sql_fuzz_func import *
+from brute_dir_backup import *
+from concurrent.futures import ThreadPoolExecutor
 
 if sys.version > '3':
     import urllib.parse as urlparse
@@ -27,13 +29,7 @@ except:
 wl_file = 'wordlist.txt'
 ext = [".php", ".txt"]
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.77 Safari/537.36'
-
-url = "http://127.0.0.1/login/"
-total_base_strings = 10
-max_tries = 7
-file = "Tools/Database/odds.json"
-debug_mode = False
-
+    
 if __name__=="__main__":
     banner()
     specify_text_color()
@@ -42,15 +38,15 @@ if __name__=="__main__":
         target = arg_parse_result.url
     
     if arg_parse_result.file:
-        file_name = arg_parse_result.file
-    else: file_name = None    
+        wordlist_name = arg_parse_result.file
+    else: wordlist_name = None    
     
     if arg_parse_result.brute_flag:
-        if file_name is not None:
+        if wordlist_name is not None:
             print("------------------------------")
             print('loading wordlist....')
             print("------------------------------\n")
-            wordlist = file_name
+            wordlist = wordlist_name
             #d_queue = create_wordlist(wordlist_name)
             print("------------------------------")
             print('loaded wordlist....')
@@ -64,26 +60,25 @@ if __name__=="__main__":
             print("------------------------------")
             print("created wordlist....")
             print("------------------------------\n")
+        #dir_scan(target, wordlist, ext)
+
+        '''
+        with ThreadPoolExecutor() as executor:
+            res_list = [executor.submit(dir_scan, target, wordlist, ext) for _ in range(10)]
+        for res in res_list:
+            print(res)
+        '''
         dir_scan(target, wordlist, ext)
         '''
-        with concurrent.futures.ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor() as executor:
             print("working...")
-            result = [executor.submit(brute_dir, d_queue, target, ext) for _ in range(10)]
-        '''    
+            result = [executor.submit(dir_scan_just, target, wordlist, ext) for _ in range(10)]    
+        '''
     elif arg_parse_result.xss_flag:
-        xss_scan(target)
+        print("xss()")
         
     elif arg_parse_result.sql_flag:
-        if not os.path.isdir("logs"):
-            os.mkdir("logs")
-            if not os.path.isfile("logs/fuzzer.log"):
-                f = open("logs/fuzzer.log", 'w')
-                f.close()
-        init_args()
-        sql_fuzz() 
-        
-    elif arg_parse_result.web_scan_flag:
-        web_scan(target)
+        print("sql injection()")
         
     else:
         usage()
